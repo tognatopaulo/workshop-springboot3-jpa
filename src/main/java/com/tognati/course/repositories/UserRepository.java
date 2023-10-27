7898365608c65c190f3e5683c0be54a4109ae341
tognatopaulo <paulo.tognato@hotmail.com>
2023-10-27 14:15:09 -0300

    Service layer, component registration

--- src/main/java/com/tognati/course/repositories/UserRepository.java
+++ src/main/java/com/tognati/course/repositories/UserRepository.java
@@ -2,6 +2,7 @@ package com.tognati.course.repositories;
 
 import com.tognati.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
